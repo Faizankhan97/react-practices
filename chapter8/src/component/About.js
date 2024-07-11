c9bb900594273f5496e5cb5eaf98897473752fde
Faizan Khan <faizikhan753@gmail.com>
2024-07-11 23:45:05 +0500

    chapter 8

--- chapter8/src/component/About.js
+++ chapter8/src/component/About.js
@@ -6,8 +6,8 @@ const About = () => {
   return (
     <>
       <h2>About</h2>
-      <User name="Faizan Khan (Function)" />
-      <UserClass name="Faizan Khan (Class)" />
+      {/* <User name="Faizan Khan (Function)" /> */}
+      <UserClass name="Faizan Khan (Class)" location="Karachi" />
     </>
   );
 };
--- chapter8/src/component/UserClass.js
+++ chapter8/src/component/UserClass.js
@@ -1,22 +1,50 @@
+import axios from "axios";
 import React from "react";
 
-class UserClass extends React.Component{
-  constructor(props){
-    super(props)
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "Dummy",
+      },
+    };
+    console.log(this.props.name + " Constructor");
+  }
+  async componentDidMount() {
+    try {
+      const res = await axios.get("https://api.github.com/users/Faizankhan97");
+      this.setState({
+        userInfo: res?.data,
+      });
+      console.log(res, "res");
+    } catch (error) {
+      console.log(error);
     }
   }
-    render(){
-      const {name, location, } = this.props;
-        return (
-          <div className="user-card">
-            <h2>Name: {name} {this.state.count}</h2>
-            <h3>Location: {location}</h3>
-            <h4>Contact: @faizikhan</h4>
-          </div>
-        );
-    }
+  componentDidUpdate() {
+    console.log("Component Did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Unmount");
+  }
+  render() {
+    console.log(this.props.name + " Render");
+    const { name, location } = this.props;
+    return (
+      <div className="user-card">
+        <h2>Name: {this?.state?.userInfo?.name}</h2>
+        <h3>Location: {this?.state?.userInfo?.location}</h3>
+        <img
+          src={this?.state?.userInfo?.avatar_url}
+          style={{ width: 100, height: 100 }}
+        />
+        <h4>Contact: @faizikhan</h4>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
